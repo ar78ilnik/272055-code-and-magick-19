6fc967e84af8f0ab62e4241b3738022c19c4af79
ar78ilnik <arakchiev.ilya@yandex.ru>
2020-01-22 15:24:15 +0300

    Criteria

--- js/stat.js
+++ js/stat.js
@@ -8,14 +8,19 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var GAP = 10;
 
-var POSITION_NAME_X = 130;
+var POSITION_NAME_X = 100;
 var POSITION_NAME_Y = 260;
 var FONT_GAP = 100;
+
+var POSITION_TIME_X = POSITION_NAME_X;
+var POSITION_TIME_Y = POSITION_NAME_Y;
 
 var POSITION_RECT_X = 100;
 var POSITION_RECT_Y = 240;
 var RECT_WIDTH = 40;
 var RECT_HEIGHT = -150;
+
+var STAT_GAP = 40;
 
 // Функция рисования облака и тени
 var renderCloud = function (ctx) {
@@ -31,7 +36,7 @@ var renderCloud = function (ctx) {
 };
 
 // Функция поиска случайного числа для отрисовки цвета столбца
-var color = function (min, max) {
+var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
@@ -52,41 +57,32 @@ window.renderStatistics = function (ctx, names, times) {
 
   var positionNameX = POSITION_NAME_X;
   var positionNameY = POSITION_NAME_Y;
+  var positionTimeX = POSITION_TIME_X;
+  var positionTimeY = POSITION_TIME_Y;
   var positionRectX = POSITION_RECT_X;
 
   //Функция поиска максимального элемента массива времен
   var maxTime = getMaxElement(times);
   maxTime = Math.floor(maxTime);
 
-  for (var i = 0; i < names.length; i++) {
-    ctx.fillText(names[i], positionNameX, positionNameY);
-    ctx.fillText(Math.round(times[i]), positionNameX, positionNameY + times[i]);
+  //Округления времен
+  for (var i = 0; i < times.length; i++) {
+    times[i] = Math.floor(times[i]);
+  }
 
+  // Цикл вывода статистики
+  for (var i = 0; i < names.length; i++) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgb(255, 0, 0)';
       POSITION_NAME_X = 130;
       POSITION_NAME_Y = 260;
     } else {
-      ctx.fillStyle = 'rgb(0, 0,' + color(MIN, MAX) + ')';
+      ctx.fillStyle = 'rgb(0, 0,' + getRandomNumber(MIN, MAX) + ')';
     }
 
-    ctx.fillText(names[i], positionNameX += 80, positionNameY);
-    ctx.fillRect(positionRectX += 80, POSITION_RECT_Y, 40, ((RECT_HEIGHT * times[i]) / maxTime));
-    ctx.fillStyle = 'black';
-
-    /*ctx.fillText('Вы', POSITION_NAME_X, POSITION_NAME_Y);
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    ctx.fillRect(POSITION_RECT_X, POSITION_RECT_Y, RECT_WIDTH, -150);
-
+    ctx.fillRect(positionRectX += STAT_GAP * 2, POSITION_RECT_Y, STAT_GAP, ((RECT_HEIGHT * times[i]) / maxTime));
     ctx.fillStyle = '#000';
-    ctx.fillText('Кекс', POSITION_NAME_X + FONT_GAP, POSITION_NAME_Y);
-    ctx.fillRect(POSITION_RECT_X + FONT_GAP, POSITION_RECT_Y, RECT_WIDTH, -150);
-
-    ctx.fillText('Катя', POSITION_NAME_X + FONT_GAP * 2, POSITION_NAME_Y);
-    ctx.fillRect(POSITION_RECT_X + FONT_GAP * 2, POSITION_RECT_Y, RECT_WIDTH, -150);
-
-    ctx.fillText('Игорь', POSITION_NAME_X + FONT_GAP * 3, POSITION_NAME_Y);
-    ctx.fillRect(POSITION_RECT_X + FONT_GAP * 3, POSITION_RECT_Y, RECT_WIDTH, -150);
-  }*/
+    ctx.fillText(names[i], positionNameX += STAT_GAP * 2, positionNameY);
+    ctx.fillText(times[i], positionTimeX += STAT_GAP * 2, 230 + ((RECT_HEIGHT * times[i]) / maxTime));
   }
 };
